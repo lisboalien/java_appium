7afc382d930e2444c74cb6058db47bcdb1a04662
Aline de Farias Lisboa <lisboalien@gmail.com>
2021-03-14 23:11:51 -0300

    First CT Trying to signup with different passwords

---
+++ src/test/java/com/test/appium/FeatureSignUp.java
@@ -0,0 +1,34 @@
+package com.test.appium;
+
+import io.appium.java_client.MobileElement;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+
+
+public class FeatureSignUp {
+
+    @Test
+    public void cannot_register_different_passwords() throws MalformedURLException {
+        AppiumDriverConfig appiumDriver = new AppiumDriverConfig();
+
+        MobileElement signUpPageButton = (MobileElement) appiumDriver.driver.findElementById("br.com.alura.aluraesporte:id/login_botao_cadastrar_usuario");
+        signUpPageButton.click();
+
+        MobileElement nameInput = (MobileElement) appiumDriver.driver.findElementById("br.com.alura.aluraesporte:id/input_nome");
+        nameInput.setValue("Aline");
+
+        MobileElement passwordInput = (MobileElement) appiumDriver.driver.findElementById("br.com.alura.aluraesporte:id/input_senha");
+        passwordInput.setValue("123");
+
+        MobileElement confirmPasswordInput = (MobileElement) appiumDriver.driver.findElementById("br.com.alura.aluraesporte:id/input_confirmar_senha");
+        confirmPasswordInput.setValue("456");
+
+        MobileElement signUpButton = (MobileElement) appiumDriver.driver.findElementById("br.com.alura.aluraesporte:id/cadastro_usuario_botao_cadastrar");
+        signUpButton.click();
+
+        MobileElement errorMessage = (MobileElement) appiumDriver.driver.findElementById("br.com.alura.aluraesporte:id/erro_cadastro");
+        Assert.assertEquals("Different Passwords error message is incorrect", "Senhas não conferem", errorMessage.getText());
+    }
+}
