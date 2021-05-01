2d5c3ed70a791a8c5f594c29aeb36d804d2e9c58
Aline de Farias Lisboa <lisboalien@gmail.com>
2021-04-30 23:25:14 -0300

    Test sign_up new user

--- src/test/java/com/test/appium/FeatureSignUp.java
+++ src/test/java/com/test/appium/FeatureSignUp.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.net.MalformedURLException;
+import java.util.NoSuchElementException;
 
 
 public class FeatureSignUp {
@@ -30,5 +31,27 @@ public class FeatureSignUp {
 
         MobileElement errorMessage = (MobileElement) appiumDriver.driver.findElementById("br.com.alura.aluraesporte:id/erro_cadastro");
         Assert.assertEquals("Different Passwords error message is incorrect", "Senhas não conferem", errorMessage.getText());
+    }
+
+    @Test
+    public void sign_up_new_user() throws NoSuchElementException, MalformedURLException {
+        AppiumDriverConfig appiumDriver = new AppiumDriverConfig();
+
+        MobileElement signUpPageButton = (MobileElement) appiumDriver.driver.findElementById("br.com.alura.aluraesporte:id/login_botao_cadastrar_usuario");
+        signUpPageButton.click();
+
+        MobileElement nameInput = (MobileElement) appiumDriver.driver.findElementById("br.com.alura.aluraesporte:id/input_nome");
+        nameInput.setValue("Aline");
+
+        MobileElement passwordInput = (MobileElement) appiumDriver.driver.findElementById("br.com.alura.aluraesporte:id/input_senha");
+        passwordInput.setValue("123");
+
+        MobileElement confirmPasswordInput = (MobileElement) appiumDriver.driver.findElementById("br.com.alura.aluraesporte:id/input_confirmar_senha");
+        confirmPasswordInput.setValue("123");
+
+        MobileElement signUpButton = (MobileElement) appiumDriver.driver.findElementById("br.com.alura.aluraesporte:id/cadastro_usuario_botao_cadastrar");
+        signUpButton.click();
+
+        MobileElement loginButton = (MobileElement) appiumDriver.driver.findElementById("br.com.alura.aluraesporte:id/login_botao_logar");
     }
 }
